8bf650f81ac1ec2f097aa503579738f843b6db18
Zaid <karaymehzaid@gmail.com>
2022-04-20 17:48:06 +0300

    0.1.0

--- api/models.py
+++ api/models.py
@@ -47,3 +47,15 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+from rest_framework.authtoken import views as auth_views
+from rest_framework.response import Response
+
+class CustomObtainToken(auth_views.ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'username': user.username, 'id': user.id, 'email': user.email})
--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_views
+from api.models import CustomObtainToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('api.urls')),
-    path('api-token-auth/', auth_views.obtain_auth_token)
+    path('api-token-auth/', CustomObtainToken)
 
 ]
